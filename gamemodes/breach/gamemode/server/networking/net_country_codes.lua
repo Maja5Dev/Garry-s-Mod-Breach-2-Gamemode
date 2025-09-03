a388e140a14ce0c9ac411faafba8663211a2e6f5
Maja552 <mayawalczynska@gmail.com>
2025-09-03 19:51:04 +0300

    Added check for ip when getting country code

--- gamemodes/breach/gamemode/server/networking/net_country_codes.lua
+++ gamemodes/breach/gamemode/server/networking/net_country_codes.lua
@@ -2,24 +2,25 @@
 function BR2_GetCountryCode(ply)
     if !ply:IsBot() then
         local ip = ply:IPAddress()
-        print(ip)
 
-        http.Fetch("http://ip-api.com/json/" .. ip .. "", 
-            function(body)
-                print(body)
+        if ip then
+            http.Fetch("http://ip-api.com/json/" .. ip .. "", 
+                function(body)
+                    print(body)
 
-                local data = util.JSONToTable(body)
+                    local data = util.JSONToTable(body)
 
-                PrintTable(data)
+                    PrintTable(data)
 
-                if data and data.countryCode then
-                    ply.CountryCode = tolower(data.countryCode)
-                    ply:SetNWString("CountryCode", tolower(data.countryCode))
+                    if data and data.countryCode then
+                        ply.CountryCode = tolower(data.countryCode)
+                        ply:SetNWString("CountryCode", tolower(data.countryCode))
+                    end
+                end,
+                function(err)
+                    print("[Breach2] Error fetching country code for player " .. ply:Nick() .. ": " .. err)
                 end
-            end,
-            function(err)
-                print("[Breach2] Error fetching country code for player " .. ply:Nick() .. ": " .. err)
-            end
-        )
+            )
+        end
     end
 end
