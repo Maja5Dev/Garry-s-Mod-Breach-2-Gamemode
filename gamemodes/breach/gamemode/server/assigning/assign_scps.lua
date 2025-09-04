2760000a7007e3b089e3316cfa992775f2011fec
Maja552 <mayawalczynska@gmail.com>
2025-09-04 12:41:44 +0300

    Moved UnDownPlayerAsZombie to assign scps

--- gamemodes/breach/gamemode/server/assigning/assign_scps.lua
+++ gamemodes/breach/gamemode/server/assigning/assign_scps.lua
@@ -66,6 +66,88 @@ function assign_system.Assign_SCP173(ply)
 	Post_Assign(ply)
 end
 
+local player_meta = FindMetaTable("Player")
+function player_meta:UnDownPlayerAsZombie(healer)
+	self.DefaultWeapons = {"weapon_scp_049_2"}
+	self.br_special_items = table.Copy(self.Body.Info.Loot)
+	self.Body.Info.Loot = {}
+
+	local rag_pos = self.Body:GetPos()
+	local lpi = self.lastPlayerInfo
+
+	if lpi then
+		lpi.PlayerTeam = TEAM_SCP
+		lpi.BreachRole = "SCP-049-2"
+		lpi.PlayerHealth = 200
+		lpi.PlayerMaxHealth = 200
+		lpi.PlayerWalkSpeed = 100
+		lpi.PlayerRunSpeed = 180
+		lpi.PlayerJumpPower = 170
+		lpi.BreachCIAgent = false
+		lpi.BreachZombie = true
+		lpi.BreachIsBleeding = false
+		lpi.can_get_infected = false
+		lpi.disable_coughing = true
+		self:ApplyPlayerInfo(lpi)
+	end
+
+	net.Start("br_update_own_info")
+		net.WriteString(self.br_showname)
+		net.WriteString(lpi.BreachRole)
+		net.WriteBool(false)
+		net.WriteBool(true)
+	net.Send(self)
+
+	self:Freeze(false)
+	self:SetMoveType(MOVETYPE_WALK)
+	self:SetNoDraw(false)
+	self:UnSpectate()
+	self:Spawn()
+
+	local spawn_pos = rag_pos
+	if IsValid(healer) then
+		spawn_pos = FindClearSpawnPos(rag_pos, healer, 32, 16, 10)
+	end
+	self:SetPos(spawn_pos)
+	
+	if IsValid(self.Body) then
+		self.Body:Remove()
+	end
+	self.br_downed = false
+	self.lastPlayerInfo = nil
+end
+
+function assign_system.Assign_SCP0492(ply)
+	Pre_Assign(ply)
+	ply:SetHealth(200)
+	ply:SetMaxHealth(200)
+	ply:SetArmor(0)
+	ply:ApplyOutfit("scp_0492")
+	ply.cantChangeOutfit = true
+	ply:Give("weapon_scp_049_2")
+	ply.use173behavior = true
+	ply.br_uses_hunger_system = false
+	ply.can_get_infected = false
+	ply.br_usesSanity = false
+	ply.br_usesTemperature = false
+	ply.canStartBleeding = false
+	ply.cantUseFlashlight = true
+	ply.br_usesStamina = false
+	ply.blinking_enabled = false
+	ply.disable_coughing = true
+	ply.br_role = "SCP-049-2"
+	ply.br_showname = "SCP-049-2"
+	ply.br_customspawn = "SPAWNS_HCZ"
+	ply.Faction = "BR2_FACTION_SCP_049"
+
+	ply:SetNWString("CPTBase_NPCFaction", "BR2_FACTION_SCP_049")
+	ply.br_support_team = SUPPORT_ROGUE
+
+	ply:SetBloodColor(DONT_BLEED)
+
+	Post_Assign(ply)
+end
+
 
 last_scp_assign = nil
 function assign_system.Assign_SCP(ply)
