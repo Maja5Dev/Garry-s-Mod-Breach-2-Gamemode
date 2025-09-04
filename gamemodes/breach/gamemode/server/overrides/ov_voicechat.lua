8fa1b9d749356cf8744e30a56c40ad00af9f9718
Maja552 <mayawalczynska@gmail.com>
2025-09-04 09:22:37 +0300

    Made chat and voicechat be global in postround

--- gamemodes/breach/gamemode/server/overrides/ov_voicechat.lua
+++ gamemodes/breach/gamemode/server/overrides/ov_voicechat.lua
@@ -1,5 +1,7 @@
 
 function GM:PlayerCanHearPlayersVoice(listener, talker)
+	if game_state == GAMESTATE_POSTROUND then return true end
+
 	local dis = listener:GetPos():Distance(talker:GetPos())
 	if listener:IsSpectator() == true or listener:Alive() == false then
 		if talker:IsSpectator() == true or talker:Alive() == false then
@@ -44,7 +46,10 @@ function GM:PlayerCanHearPlayersVoice(listener, talker)
 end
 
 function GM:PlayerCanSeePlayersChat(text, teamOnly, listener, talker)
-	if !IsValid(talker) or talker == listener then return true end
+	if game_state == GAMESTATE_POSTROUND then return true end
+	if !IsValid(talker) or !IsValid(listener) then return false end
+	if talker == listener then return true end
+
 	local dis = listener:GetPos():Distance(talker:GetPos())
 	if listener:IsSpectator() == true or listener:Alive() == false then
 		if talker:IsSpectator() == true or talker:Alive() == false then
