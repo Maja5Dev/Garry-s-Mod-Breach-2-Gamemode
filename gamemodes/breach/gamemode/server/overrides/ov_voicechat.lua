c2216fc5801ff763ba873486537f42be6424e8e7
Maja552 <mayawalczynska@gmail.com>
2025-09-09 18:12:06 +0300

    Fixed downed people being able to voice chat and text

--- gamemodes/breach/gamemode/server/overrides/ov_voicechat.lua
+++ gamemodes/breach/gamemode/server/overrides/ov_voicechat.lua
@@ -2,6 +2,8 @@
 function GM:PlayerCanHearPlayersVoice(listener, talker)
 	if game_state == GAMESTATE_POSTROUND then return true end
 
+	if talker.br_downed then return false
+
 	local dis = listener:GetPos():Distance(talker:GetPos())
 	if listener:IsSpectator() == true or listener:Alive() == false then
 		if talker:IsSpectator() == true or talker:Alive() == false then
@@ -49,6 +51,7 @@ function GM:PlayerCanSeePlayersChat(text, teamOnly, listener, talker)
 	if game_state == GAMESTATE_POSTROUND then return true end
 	if !IsValid(talker) or !IsValid(listener) then return false end
 	if talker == listener then return true end
+	if talker.br_downed then return false
 
 	local dis = listener:GetPos():Distance(talker:GetPos())
 	if listener:IsSpectator() == true or listener:Alive() == false then
