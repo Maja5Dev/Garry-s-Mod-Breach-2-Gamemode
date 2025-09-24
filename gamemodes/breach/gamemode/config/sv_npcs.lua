7bccb6ea2ac57b6d89fd843c04c065c6af63b2ff
Maja552 <mayawalczynska@gmail.com>
2025-09-24 15:45:24 +0300

    Disabled 096 for now

--- gamemodes/breach/gamemode/config/sv_npcs.lua
+++ gamemodes/breach/gamemode/config/sv_npcs.lua
@@ -17,6 +17,7 @@ BR_DISABLE_NPCS = {
     ["npc_cpt_scp_575"] = true,
     ["npc_cpt_scp_173"] = true,
     ["npc_cpt_scp_575"] = true,
+    ["npc_cpt_scp_096"] = true,
 }
 
 BLUE_NVG_ENTITY_NAMES = {
