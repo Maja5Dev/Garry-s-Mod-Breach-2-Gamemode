7345a8261803e71f36f793d16615e2a78b5b087a
Maja552 <mayawalczynska@gmail.com>
2025-09-24 16:19:25 +0300

    Fixed scp 173 bug

--- gamemodes/breach/gamemode/server/networking/net_scp.lua
+++ gamemodes/breach/gamemode/server/networking/net_scp.lua
@@ -1,6 +1,7 @@
 
 net.Receive("br_scp173_mode", function(len, ply)
 	if len < 200 and ply.br_role == "SCP-173" then
+		local wep = ply:GetActiveWeapon()
 		wep:HandleMovementModeToggle()
 	end
 end)
