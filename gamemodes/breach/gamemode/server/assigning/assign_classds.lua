a8008d2ab5af58500268a70cb23041f21301df49
Maja552 <mayawalczynska@gmail.com>
2025-10-01 12:17:56 +0300

    Made it so class d numbers are always random

--- gamemodes/breach/gamemode/server/assigning/assign_classds.lua
+++ gamemodes/breach/gamemode/server/assigning/assign_classds.lua
@@ -1,24 +1,47 @@
 
+local AssignedNumbers = {}
+
+local function GenerateUniqueNumber()
+    local num
+    repeat
+        num = math.random(1000, 9999) -- generates 4-digit number
+    until not AssignedNumbers[num]    -- repeat if already taken
+
+    AssignedNumbers[num] = true       -- mark as used
+    return num
+end
+
 function assign_system.Assign_ClassD(ply)
 	Pre_Assign(ply)
 	ply:ApplyOutfit("class_d")
 	ply:Give("br_hands")
-	--local doc = ply:Give("br2_doc_classd")
-	ply.br_showname = "D-" ..math.random(1,9)..math.random(0,9)..math.random(0,9)..math.random(0,9) .. ""
-	--if ply.br_showname == "D-9341" then
-	--	ply.isTheOne = true
-	--end
-	/*
-	if IsValid(doc) then
-		doc:SetNWString("NW_Dcode", ply.br_showname)
+
+	local d9341_exists = false
+	for k,v in pairs(player.GetAll()) do
+		if v.br_showname == "D-9341" then
+			d9341_exists = true
+			break
+		end
 	end
-	*/
+
+	if !d9341_exists and math.random(1,50) == 21 then
+		ply.br_showname = "D-9341"
+	else
+		local uniqueNum = GenerateUniqueNumber()
+        ply.br_showname = "D-" .. uniqueNum
+	end
+
+	if ply.br_showname == "D-9341" then
+		ply.isTheOne = true
+	end
+
 	ply.br_hunger = 90
 	ply.br_thirst = 90
 	ply.br_sanity = math.random(85, 100)
 	ply.br_role = "Class D"
 	ply.br_usesSanity = true
 	ply.br_usesTemperature = true
+
 	if ply.support_spawning == false then
 		ply.br_support_spawns = {{"class_d", 1}, {"mtf", 1}}
 		ply.first_info = "classd"
